7dfdd32fd838d790fedc899e69e5f1fd94aa4283
Bessma <bessma@bessma.bessma>
2025-07-20 15:53:32 +0200

    Premier commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by User on 20/07/2025
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
